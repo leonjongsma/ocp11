0ca7f194f409a29457a35026bc287acfb069425f
Leon Jongsma <ljongsma@bebr.nl>
2020-10-27 22:36:52 +0100

    added some code to try out

---
+++ java-object-oriented-approach/src/main/java/nl/leon/ocp/javaobjectorientedapproach/polymorphism/HasTail.java
@@ -0,0 +1,8 @@
+package nl.leon.ocp.javaobjectorientedapproach.polymorphism;
+
+/**
+ * @author : Leon Jongsma
+ */
+public interface HasTail {
+    public abstract boolean isTailStriped();
+}
---
+++ java-object-oriented-approach/src/main/java/nl/leon/ocp/javaobjectorientedapproach/polymorphism/Primate.java
@@ -0,0 +1,10 @@
+package nl.leon.ocp.javaobjectorientedapproach.polymorphism;
+
+/**
+ * @author : Leon Jongsma
+ */
+public class Primate {
+    public boolean hasHair() {
+        return true;
+    }
+}
