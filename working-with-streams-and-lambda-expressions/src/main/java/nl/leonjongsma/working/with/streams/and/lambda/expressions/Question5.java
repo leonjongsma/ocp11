af840a58827806cb4dc19a1407b46a6c6d7520a0
Leon Jongsma <l.jongsma@tfe.nl>
2022-07-15 15:38:22 +0200

    15-6: Question #31 of 50

--- working-with-streams-and-lambda-expressions/src/main/java/nl/leonjongsma/working/with/streams/and/lambda/expressions/Question5.java
+++ working-with-streams-and-lambda-expressions/src/main/java/nl/leonjongsma/working/with/streams/and/lambda/expressions/Question5.java
@@ -4,11 +4,7 @@ import java.util.function.BiFunction;
 
 public class Question5 {
     public static void main(String[] args) {
-        BiFunction<String, Number, String> funct1 = (final String s, Number n) -> String.format(s,n);
+        BiFunction<String, Number, String> funct1 = (final String s, Number n) -> String.format(s, n);
         BiFunction<String, Number, String> funct2 = (s, n) -> s + n;
-
-
-
-
     }
 }
